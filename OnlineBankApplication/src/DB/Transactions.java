a28dda02fbccb3a388663ca8134a00a1c697ebd7
Dhanushverthanan <verthanandhanush@gmail.com>
2026-01-28 23:10:54 +0530

    Create Transactions.java

---
+++ OnlineBankApplication/src/DB/Transactions.java
@@ -0,0 +1,27 @@
+package DB;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Transactions {
+
+	public static void withrawal() throws SQLException
+	{
+		Connection con = ConnectionSql.getConnections();
+		String qury ="select * from users;";
+		PreparedStatement pre = con.prepareStatement(qury);
+		ResultSet rs =pre.executeQuery();
+		 while (rs.next()) 
+		 {
+	            System.out.println(rs.getInt(1) + " " + rs.getString(2));
+	     }
+	 }
+	
+	public static void main(String[] args) throws SQLException {
+		withrawal();
+
+	}
+
+}
