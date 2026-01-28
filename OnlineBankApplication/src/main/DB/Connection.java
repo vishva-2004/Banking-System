84d9604b72cce04e68d9b694736c564f0cf512be
Dhanushverthanan <verthanandhanush@gmail.com>
2026-01-28 21:15:50 +0530

    Update Connection.java

--- OnlineBankApplication/src/main/DB/Connection.java
+++ OnlineBankApplication/src/main/DB/Connection.java
@@ -2,5 +2,10 @@ package DB;
 
 class Connection 
 {
-	
+	public static Connection getConnections()	{
+		String url ="jdbc:Mysql://localhost:3306/bankingapp";
+		String name ="noconnection";
+		String password="Dhanush2295@";
+		return DriverManager.getConnection(url,name,pass);
+	}
 }
