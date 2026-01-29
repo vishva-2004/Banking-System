18a846d87f448a4a001a1202e54d418eb2c58211
Dhanushverthanan <verthanandhanush@gmail.com>
2026-01-29 14:40:17 +0530

    Create Account_creation.java

---
+++ OnlineBankApplication/src/User_Interface/Account_creation.java
@@ -0,0 +1,13 @@
+package User_Interface;
+
+import java.util.ArrayList;
+
+public class Account_creation {
+	ArrayList<User> users = new ArrayList<>();
+	
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
