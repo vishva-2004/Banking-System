1c8626829e2edd133f8dabf21b5ae1c329d21ce9
Dhanushverthanan <verthanandhanush@gmail.com>
2026-01-29 14:40:02 +0530

    Create User.java

---
+++ OnlineBankApplication/src/User_Interface/User.java
@@ -0,0 +1,51 @@
+package User_Interface;
+
+public class User {
+
+	private int user_id;
+	private String name;
+	private String email;
+	private String password;
+	private String phoneno;
+
+	public int getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPhoneno() {
+		return phoneno;
+	}
+
+	public void setPhoneno(String phoneno) {
+		this.phoneno = phoneno;
+	}
+
+}
