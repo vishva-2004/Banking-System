a62317c3ec6ba67cc120a98ee41048a4c7eb827f
Dhanushverthanan <verthanandhanush@gmail.com>
2026-01-31 00:58:56 +0530

    Update Transactions.java

--- OnlineBankApplication/src/DB/Transactions.java
+++ OnlineBankApplication/src/DB/Transactions.java
@@ -4,22 +4,23 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Transactions {
 
+	public static Scanner sc = new Scanner(System.in);
 	public static void Account_creation() throws SQLException
 	{
 		Connection con = ConnectionSql.getConnections();
-		
-		
 	}
 	
 	
 	public static void withrawal() throws SQLException
 	{
 		Connection con = ConnectionSql.getConnections();
-		String qury ="select * from users;";
+		String qury ="select * from accounts where account_no=?;";
 		PreparedStatement pre = con.prepareStatement(qury);
+		pre.setInt(1, sc.nextInt());
 		ResultSet rs =pre.executeQuery();
 		 while (rs.next()) 
 		 {
